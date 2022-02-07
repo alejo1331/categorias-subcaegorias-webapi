2d349a6d52ab2a9df9d60b2f24e46a51c84d0b8e
Mauricio Torres Escobar <mtorresescobar@yahoo.es>
2022-02-07 14:17:00 -0500

    Se migra API a Net 6.0

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,25 +2,84 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
+using Domain.Data;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Domain.Bussiness.Profiles;
+using Microsoft.OpenApi.Models;
 
-namespace Api
+var builder = WebApplication.CreateBuilder(args);
+//public IConfiguration Configuration { get; }
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(AdministracionProfile));
+
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.AddPolicy("CorsPolicy", builder =>
+    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+AddDatabaseConfiguration(ref builder);
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}    
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
+
+bool IsDevelopmentEnvironment(string env) => env == "Development";
+
+bool IsStagingEnvironment(string env) => env == "Staging";
+
+bool IsProductionEnvironment(string env) => env == "Production";
+
+void AddDatabaseConfiguration(ref WebApplicationBuilder builder)
+{
+    builder.Services.AddEntityFrameworkSqlServer().AddDbContext<Context>(options =>
+                options.UseSqlServer(("DefaultConnection")));
+
+};
+
+
+
